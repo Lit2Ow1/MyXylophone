038e9e08c322fcde941f459438a7d1d7ca8e9d17
Alex Baranov <killzone15187@gmail.com>
2023-01-30 16:12:24 +0300

    icon

--- MyXylophone/ViewController.swift
+++ MyXylophone/ViewController.swift
@@ -6,14 +6,35 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+ 
+        
+     @IBAction func keyPressed(_ sender: UIButton) {
+     
+     
+        playSound(soundName: sender.currentTitle!)
+        
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1.0
+        }
+        
+    }
+    
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+        
+    }
+    
 }
-
